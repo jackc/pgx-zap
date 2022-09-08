1327d674b35573e0333cb56506bb7be35399da73
Lauris BH <lauris@nix.lv>
2022-09-08 15:11:42 +0300

    Fix zap logging adapter to work with latest pgx v5

--- adapter.go
+++ adapter.go
@@ -4,7 +4,7 @@ package zap
 import (
 	"context"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/tracelog"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
@@ -17,7 +17,7 @@ func NewLogger(logger *zap.Logger) *Logger {
 	return &Logger{logger: logger.WithOptions(zap.AddCallerSkip(1))}
 }
 
-func (pl *Logger) Log(ctx context.Context, level pgx.LogLevel, msg string, data map[string]interface{}) {
+func (pl *Logger) Log(ctx context.Context, level tracelog.LogLevel, msg string, data map[string]interface{}) {
 	fields := make([]zapcore.Field, len(data))
 	i := 0
 	for k, v := range data {
@@ -26,15 +26,15 @@ func (pl *Logger) Log(ctx context.Context, level pgx.LogLevel, msg string, data 
 	}
 
 	switch level {
-	case pgx.LogLevelTrace:
+	case tracelog.LogLevelTrace:
 		pl.logger.Debug(msg, append(fields, zap.Stringer("PGX_LOG_LEVEL", level))...)
-	case pgx.LogLevelDebug:
+	case tracelog.LogLevelDebug:
 		pl.logger.Debug(msg, fields...)
-	case pgx.LogLevelInfo:
+	case tracelog.LogLevelInfo:
 		pl.logger.Info(msg, fields...)
-	case pgx.LogLevelWarn:
+	case tracelog.LogLevelWarn:
 		pl.logger.Warn(msg, fields...)
-	case pgx.LogLevelError:
+	case tracelog.LogLevelError:
 		pl.logger.Error(msg, fields...)
 	default:
 		pl.logger.Error(msg, append(fields, zap.Stringer("PGX_LOG_LEVEL", level))...)
